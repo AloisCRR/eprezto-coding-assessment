b0ccca0d892f99508c34d9a85cd26c2a29936b0e
Alois Carrera <aloiscarrera@gmail.com>
2021-07-18 15:41:29 -0500

    feat: Project begins

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Eprezto Coding Assessment</title>
+        <meta
+          name="description"
+          content="Project for Eprezto Coding Assessment v1.0"
+        />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <div>Eprezto Coding Assessment</div>
+    </>
+  );
+}
