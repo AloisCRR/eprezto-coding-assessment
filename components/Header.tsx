8d157bb83025871431828f38eeee11724e724e06
Alois Carrera <aloiscarrera@gmail.com>
2021-07-19 12:30:43 -0500

    refactor: :lipstick: Center company logo on small screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ interface Props {}
 export default function Header({}: Props) {
   return (
     <header className="pt-12 text-white flex-col">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-center sm:justify-between">
         <Image src={EpreztoLogo} width={140} height={30} alt="Company logo" />
         <span className="hidden font-hind tracking-widest sm:inline">
           Eprezto Coding Assessment v1.0 | Alois Carrera
